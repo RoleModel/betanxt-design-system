2e6fe7efdebd9b349f25b9dadc28924661e597bf
Ben Eiwnechter <ben.einwechter@rolemodelsoftware.com>
2025-05-14 10:29:56 -0400

    Re-enable charts styling

--- src/themes/base/components/x-charts.ts
+++ src/themes/base/components/x-charts.ts
@@ -1,25 +1,22 @@
-// TODO: Type augmentation doesn't seeem to work anymore after updating to MUI-X v8. Figure out why and then uncomment this file.
+import type { ThemeOptions } from '@mui/material/styles'
+import type {} from '@mui/x-charts/themeAugmentation'
 
-// import type { ThemeOptions } from '@mui/material/styles'
-// import type {} from '@mui/material/themeCssVarsAugmentation'
-// import type {} from '@mui/x-charts/themeAugmentation'
-
-// export const components: ThemeOptions['components'] = {
-//   MuiAreaElement: {
-//     styleOverrides: {
-//       root: {
-//         opacity: 0.2,
-//       },
-//     },
-//   },
-//   MuiChartsTooltip: {
-//     styleOverrides: {
-//       cell: ({ theme }) => ({
-//         fontSize: theme.typography.body3.fontSize,
-//         '.MuiTypography-root': {
-//           fontSize: theme.typography.body3.fontSize,
-//         },
-//       }),
-//     },
-//   },
-// }
+export const components: ThemeOptions['components'] = {
+  MuiAreaElement: {
+    styleOverrides: {
+      root: {
+        opacity: 0.2,
+      },
+    },
+  },
+  MuiChartsTooltip: {
+    styleOverrides: {
+      cell: ({ theme }) => ({
+        fontSize: theme.typography.body3.fontSize,
+        '.MuiTypography-root': {
+          fontSize: theme.typography.body3.fontSize,
+        },
+      }),
+    },
+  },
+}

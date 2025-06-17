f1b68abfaf5acf1b687d7240ee0b87a03abc07b8
Dallas Peters <dallas.peters@rolemodelsoftware.com>
2025-06-17 12:00:06 -0500

    fix BNLogo andBNAppFooter

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,9 @@ export type { BNChartGradientFillProps, BNSeriesGradient } from './BNChartGradie
 export { BNAppBar } from './app-bar/BNAppBar'
 export { BNAppSwitcher } from './BNAppSwitcher'
 
+export { BNLogo } from './BNLogo'
+export type { BNLogoProps } from './BNLogo'
+
 export { BNAppFooter } from './BNAppFooter'
 export { BNStatCard } from './BNStatCard'
 export { BNSelectCard } from './BNSelectCard'

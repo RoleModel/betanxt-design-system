9768553b02fe0d41b28b3d0bc29b147133e9097f
Dallas Peters <dallas.peters@rolemodelsoftware.com>
2025-06-20 08:36:34 -0500

    Fix-toggle-button-accessibility (#15)
    * fix accessibility of toggle button group color.
    
    * prettier
    
    * Bump version

---
+++ src/stories/ToggleButton.stories.tsx
@@ -0,0 +1,62 @@
+import { type Meta, type StoryObj } from '@storybook/react'
+import React from 'react'
+
+import { ToggleButton, ToggleButtonGroup } from '@mui/material'
+
+const meta: Meta<typeof ToggleButtonGroup> = {
+  title: 'Components/ToggleButtonGroup',
+  component: ToggleButtonGroup,
+  parameters: {
+    layout: 'centered',
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/w1pqRAs10H0goKjxJl6HES/MUI-v6.1.0?node-id=11011-143217&t=njdQUyzVttt456w5-11',
+    },
+  },
+  argTypes: {
+    color: {
+      control: 'select',
+      options: ['primary', 'secondary', 'error', 'info', 'success', 'warning', 'inherit'],
+    },
+    size: {
+      control: 'select',
+      options: ['small', 'medium', 'large'],
+    },
+    children: {
+      control: 'text',
+    },
+    onClick: { action: 'clicked' },
+  },
+  args: {
+    children: 'ToggleButton',
+    color: 'secondary',
+    size: 'large',
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof ToggleButtonGroup>
+
+export const ToggleButtonGroupComponent: Story = {
+  render: (args) => {
+    const [view, setView] = React.useState('list')
+
+    const handleChange = (_event: React.MouseEvent<HTMLElement>, nextView: string) => {
+      setView(nextView)
+    }
+
+    return (
+      <ToggleButtonGroup {...args} value={view} exclusive onChange={handleChange}>
+        <ToggleButton value="left" aria-label="left aligned">
+          Left
+        </ToggleButton>
+        <ToggleButton value="center" aria-label="centered">
+          Center
+        </ToggleButton>
+        <ToggleButton value="right" aria-label="right aligned">
+          Right
+        </ToggleButton>
+      </ToggleButtonGroup>
+    )
+  },
+}

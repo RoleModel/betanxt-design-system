6b8683436b82df65fab8d6a86f0104d621c949a6
Ben Eiwnechter <ben.einwechter@rolemodelsoftware.com>
2025-11-25 11:18:49 -0500

    Add BNHamburgerMenu

---
+++ src/components/app-header/BNHamburgerMenu.tsx
@@ -0,0 +1,144 @@
+import { type MouseEvent, type ReactNode, useRef, useState } from 'react'
+
+import {
+  Avatar,
+  type AvatarProps,
+  type CSSObject,
+  ClickAwayListener,
+  Icon,
+  IconButton,
+  ListSubheader,
+  MenuList,
+  Paper,
+  Popper,
+  keyframes,
+  styled,
+} from '@mui/material'
+
+// Define Keyframes
+const bottombarOpen = keyframes`
+  0% { top: 19px; }
+  50% { top: 12px; transform: rotate(0deg); }
+  100% { top: 12px; transform: rotate(-45deg); }
+`
+
+const bottombarClose = keyframes`
+  0% { top: 12px; transform: rotate(-45deg); }
+  50% { top: 12px; transform: rotate(0deg); }
+  100% { top: 19px;}
+`
+
+const topbarOpen = keyframes`
+  0% { top: 9px; }
+  50% { top: 12px; transform: rotate(0deg); }
+  100% { top: 12px; transform: rotate(45deg); }
+`
+
+const topbarClose = keyframes`
+  0% { top: 12px; transform: rotate(45deg); }
+  50% { top: 12px; transform: rotate(0deg); }
+  100% { top: 9px; }
+`
+
+const StyledMenuIcon = styled('button')((): CSSObject => {
+  return {
+    border: 'none',
+    margin: 0,
+    padding: 0,
+    overflow: 'visible',
+    background: 'transparent',
+    color: 'inherit',
+    font: 'inherit',
+    lineHeight: 'normal',
+    appearance: 'none',
+    outline: 'none',
+    cursor: 'pointer',
+    position: 'relative',
+    width: '28px',
+    height: '28px',
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    borderRadius: '50%',
+    top: 0,
+    '&:focus': {
+      outline: '2px solid currentColor',
+      outlineOffset: '2px',
+    },
+    '&:focus:not(:focus-visible)': {
+      outline: 'none',
+    },
+    '& div': {
+      display: 'block',
+      position: 'absolute',
+      height: 2,
+      width: '100%',
+      background: 'currentColor',
+      opacity: 1,
+      left: 0,
+      transformOrigin: 'center center',
+      willChange: 'transform, top',
+    },
+    '&[data-open="true"] div:nth-of-type(1)': {
+      animation: `${topbarOpen} 0.65s ease forwards`,
+    },
+    '&[data-open="false"] div:nth-of-type(1)': {
+      animation: `${topbarClose} 0.65s ease forwards`,
+    },
+    '&[data-open="true"] div:nth-of-type(2)': {
+      animation: `${bottombarOpen} 0.65s ease forwards`,
+    },
+    '&[data-open="false"] div:nth-of-type(2)': {
+      animation: `${bottombarClose} 0.65s ease forwards`,
+    },
+  }
+})
+
+export type BNHamburgerMenuProps = Pick<AvatarProps, 'src' | 'srcSet' | 'alt'> & {
+  children?: ReactNode
+}
+
+export function BNHamburgerMenu(props: BNHamburgerMenuProps) {
+  const [isOpen, setIsOpen] = useState(false)
+  const buttonRef = useRef<HTMLButtonElement>(null)
+
+  const toggleMenu = (event: MouseEvent<HTMLElement>) => {
+    setIsOpen((prev) => !prev)
+  }
+
+  const closeMenu = () => {
+    setIsOpen(false)
+  }
+
+  return (
+    <ClickAwayListener onClickAway={closeMenu}>
+      <div>
+        <StyledMenuIcon
+          ref={buttonRef}
+          data-open={isOpen}
+          onClick={toggleMenu}
+          aria-label={isOpen ? 'Close menu' : 'Open menu'}
+          aria-expanded={isOpen}
+          type="button"
+        >
+          <div />
+          <div />
+        </StyledMenuIcon>
+        {isOpen && buttonRef.current && (
+          <Popper
+            role="navigation"
+            id="avatar-menu"
+            open={isOpen}
+            anchorEl={buttonRef.current}
+            placement="bottom-end"
+            sx={{ zIndex: (theme) => theme.zIndex.appBar + 1 }}
+            modifiers={[{ name: 'offset', options: { offset: [0, 18] } }]}
+          >
+            <Paper sx={{ maxHeight: '100vh', overflowY: 'auto' }}>
+              <div>{props.children}</div>
+            </Paper>
+          </Popper>
+        )}
+      </div>
+    </ClickAwayListener>
+  )
+}
